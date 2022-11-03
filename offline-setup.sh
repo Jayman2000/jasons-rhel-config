5093ac9953d85a947bef04b0af0690210af1492a
Jason Yundt <jason@jasonyundt.email>
2022-11-03 14:20:50 -0400

    Install offline-setup.sh using Bash instead of Ansible

--- offline-setup.sh
+++ offline-setup.sh
@@ -4,3 +4,15 @@
 declare -r git_config=( sudo -u jayman git config --global )
 "${git_config[@]}" user.name "Jason Yundt"
 "${git_config[@]}" user.email "jason@jasonyundt.email"
+
+declare -r installation_path=/usr/local/bin/offline-setup.sh
+# Thanks, Hiks Gerganov
+# <https://www.baeldung.com/linux/bash-get-location-within-script#bash-script-location>
+declare -r this_scripts_path="${BASH_SOURCE}"
+if [ "$this_scripts_path" != "$installation_path" ]
+then
+	touch "$installation_path"
+	chown root:root "$installation_path"
+	chmod u=rx,g=,o= "$installation_path"
+	cp --no-preserve=all "$this_scripts_path" "$installation_path"
+fi
