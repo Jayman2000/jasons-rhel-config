3179f19e14820ebae003760360ac786325fee238
Jason Yundt <jason@jasonyundt.email>
2022-11-03 11:55:34 -0400

    Configure Git using Bash instead of Ansible

--- offline-setup.sh
+++ offline-setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # This file is for setup tasks that should succeed even if the system
 # has no network connection.
-echo test > /root/test
+declare -r git_config=( sudo -u jayman git config --global )
+"${git_config[@]}" user.name "Jason Yundt"
+"${git_config[@]}" user.email "jason@jasonyundt.email"
