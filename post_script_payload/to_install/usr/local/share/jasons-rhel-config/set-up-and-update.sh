7d69a319a6967aa999f7d3f780da78c90195769b
Jason Yundt <jason@jasonyundt.email>
2022-11-07 19:07:08 -0500

    Make set-up-and-update update RPM packages

--- post_script_payload/to_install/usr/local/share/jasons-rhel-config/set-up-and-update.sh
+++ post_script_payload/to_install/usr/local/share/jasons-rhel-config/set-up-and-update.sh
@@ -8,3 +8,20 @@ then
 	# good enough.
 	rm /etc/systemd/default.target
 fi
+
+dnf install --nobest --assumeyes PackageKit
+
+pkcon update --noninteractive --only-download
+pkcon_exit_status="$?"
+
+# According to pkcon(1), exit status 5 means “Nothing useful was done.”
+# For the purposes of this script, that shouldn’t count as an error.
+if [ "$pkcon_exit_status" = 0 ] || [ "$pkcon_exit_status" = 5 ]
+then
+        pkcon offline-trigger --noninteractive
+        pkcon_exit_status="$?"
+        if [ "$pkcon_exit_status" = 5 ]
+        then
+                exit 0
+        fi
+exit "$pkcon_exit_status"
