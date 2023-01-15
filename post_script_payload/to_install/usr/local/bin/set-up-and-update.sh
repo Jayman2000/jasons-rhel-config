9eaabc02be8f7ed8186d68a19628af51dafc646a
Jason Yundt <jason@jasonyundt.email>
2023-01-15 11:06:24 -0500

    Set up Syncthing
    Implements <mid:20230103235608.4rp3lb5ysk533hpr@Jason-Desktop-Linux>.

--- post_script_payload/to_install/usr/local/bin/set-up-and-update.sh
+++ post_script_payload/to_install/usr/local/bin/set-up-and-update.sh
@@ -9,6 +9,11 @@ then
 	rm /etc/systemd/default.target
 fi
 
+function echo_err
+{
+	echo "$@" 1>&2
+}
+
 declare -r dnf_install=( dnf install --nobest --assumeyes )
 function potentially_install
 {
@@ -49,6 +54,7 @@ potentially_install \
 	PackageKit \
 	@"Server with GUI" \
 	python \
+	syncthing \
 	virt-manager
 
 sudo -u jayman gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
@@ -58,6 +64,82 @@ declare -r git_config=( sudo -u jayman git config --global )
 "${git_config[@]}" user.name 'Jason Yundt'
 "${git_config[@]}" user.email jason@jasonyundt.email
 "${git_config[@]}" alias.f 'fetch --all --prune'
+
+# If syncthing@jayman isn’t enabled, then assume that we haven’t set up Syncthing yet.
+if systemctl is-enabled syncthing@jayman | grep disabled > /dev/null
+then
+	readonly syncthing=( sudo -u jayman syncthing )
+	if "${syncthing[@]}" generate --no-default-folder
+	then
+		# The --now is necessary. Syncthing needs to be running in order to add devices.
+		if systemctl enable --now syncthing@jayman
+		then
+			# Syncthing needs a second to start. Wait for Syncthing’s HTTP API to be up…
+			api_available=0
+			tries=0
+			readonly max_tries=60
+			readonly seconds_before_retrying=1
+			while [ "$tries" -lt "$max_tries" ]
+			do
+				sleep 1s
+				if "${syncthing[@]}" cli show system &> /dev/null
+				then
+					# The Syncthing HTTP API is now up!
+					api_available=1
+					break
+				fi
+				let tries++
+			done
+
+			if [ "$api_available" -eq 1 ]
+			then
+				readonly folder_ids=(
+					syrpl-vpqnk  # .save
+					eheef-uq5hv  # Game Data
+					mjwge-zeznc  # Projects
+				)
+				readonly syncthing_config=( "${syncthing[@]}" cli config )
+				for folder_id in "${folder_ids[@]}"
+				do
+					"${syncthing_config[@]}" folders add --id "$folder_id" --path "~/Documents/Home/Syncthing/$folder_id"
+				done
+
+				readonly device_ids=(
+					AEU6Q56-L5J3AGY-Z4H6S4A-JZH6VPO-DXI66VM-GFBDSGT-CQQTNMY-TKH6CQY  # Graphical-Test-VM
+					7A735CO-FSRRF2I-FN5WRGV-OHGRWHR-TF4Z47H-OJBHRBA-G7CP7BN-FTLXGAX  # Jason-Desktop-Linux
+					DAW6JNR-DHBHAVL-42UVJDB-SENEDDQ-OVLHNH3-XOVKDE4-JXVIQ23-GJBG6QZ  # Jason-Desktop-Windows
+					HIUQOJU-CNAGZCU-BHAFKP7-2T4WAO3-XUMWZKC-N2ZXQWD-XSGWNZH-WRGEWAP  # Jason-Laptop-Linux
+					QZBHFNE-XJWGGY4-6JXYMD3-D3HVGR2-C64BVH2-6M644XU-RSVRGAS-QZ752Q7  # Server
+				)
+				for device_id in "${device_ids[@]}"
+				do
+					if "${syncthing_config[@]}" devices add --device-id "$device_id"
+					then
+						for folder_id in "${folder_ids[@]}"
+						do
+							if ! "${syncthing_config[@]}" folders "$folder_id" devices add --device-id "$device_id"
+							then
+								echo_err "Failed to share a folder ($folder_id) with a device ($device_id)."
+							fi
+						done
+					else
+						echo_err "Failed to add Syncthing device: $device_id."
+					fi
+				done
+			else
+				echo_err "syncthing@jayman sucessfully started, but the HTTP API never became available. Disabling syncthing@jayman to make this error more noticable…"
+				if ! systemctl disable syncthing@jayman
+				then
+					echo_err "Failed to disable syncthing@jayman."
+				fi
+			fi
+		else
+			echo_err "Failed to enable syncthing@jayman."
+		fi
+	else
+		echo_err "Failed to generate Syncthing config."
+	fi
+fi
 
 pkcon refresh --noninteractive
 es="$?"
